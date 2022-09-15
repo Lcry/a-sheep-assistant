8b88a1b4784855954520a125f2d6bd80a4fcc7eb
ᴸᶜʳʸ <25002259+Lcry@users.noreply.github.com>
2022-09-15 12:15:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 # -*- coding: utf-8 -*-
 
+
 """
 @author : lcry
-@time : 2022/9/14 15:10
+@time : 2022/9/15 12:00
 """
+# 接口地址,请求头参数必填:t
+map_api = "https://cat-match.easygame2021.com/sheep/v1/game/map_info?map_id=%s"
+finish_api = "https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=%s&rank_time=%s&rank_role=1&skin=1"
+
+
+# 完成闯关
+def finish_game(state, cost_time):
+    pass
 
 
 if __name__ == '__main__':
-    pass
+    # 第一关map_id=80001
+    # 第二关map_id=90015
+    print("开始闯关...")
+    print(map_api % 80001)
+    print(finish_api % (1, 60))
+    finish_game(1, 60)
+    print("闯关成功...")
