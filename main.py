0e59b8f351ddd8e14e1a856ea997e2c9931028eb
Lcry <i@51it.wang>
2022-09-16 19:19:33 +0800

    升级V2022.1.0Beta,新增羊群和话题开关控制,针对接口限流随机睡眠

--- main.py
+++ main.py
@@ -5,39 +5,62 @@
 """
 import random
 import sys
+import time
 
 import requests
 
 import config
 
 map_api = "https://cat-match.easygame2021.com/sheep/v1/game/map_info?map_id=%s"
-# 完成游戏接口 需要参数状态以及耗时（单位秒）
-finish_api = "https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=%s&rank_time=%s&rank_role=1&skin=1"
+# 完成羊群接口
+finish_sheep_api = "https://cat-match.easygame2021.com/sheep/v1/game/game_over?rank_score=1&rank_state=1&rank_time=%s&rank_role=1&skin=%s"
+# 完成话题接口
+finish_topic_api = "https://cat-match.easygame2021.com/sheep/v1/game/topic_game_over?rank_score=1&rank_state=1&rank_time=%s&rank_role=2&skin=%s"
 
 header_t = config.get("header_t")
 header_user_agent = config.get("header_user_agent")
 cost_time = config.get("cost_time")
 cycle_count = config.get("cycle_count")
+sheep_type = config.get("sheep_type")
+topic_type = config.get("topic_type")
 
 request_header = {
     "Host": "cat-match.easygame2021.com",
     "User-Agent": header_user_agent,
-    "t": header_t
+    "t": header_t,
+    "Referer": "https://servicewechat.com/wx141bfb9b73c970a9/17/page-frame.html",
+    "Accept-Encoding": "gzip,compress,br,deflate"
 }
 
 """
-调用完成闯关
+调用完成闯关羊群
 Parameters:
-  state - 状态
   cost_time - 耗时
 """
 
 
-def finish_game(state, rank_time):
-    res = requests.get(finish_api % (state, rank_time), headers=request_header, timeout=6)
+def finish_game_sheep(skin, rank_time):
+    res = requests.get(finish_sheep_api % (rank_time, skin), headers=request_header, timeout=10, verify=True)
     # err_code为0则成功
     if res.json()["err_code"] == 0:
-        print("状态成功")
+        print("闯关羊群状态成功")
+    else:
+        print(res.json())
+        print("请检查t的值是否获取正确!")
+
+
+"""
+调用完成闯关话题
+Parameters:
+  cost_time - 耗时
+"""
+
+
+def finish_game_topic(skin, rank_time):
+    res = requests.get(finish_topic_api % (rank_time, skin), headers=request_header, timeout=10, verify=True)
+    # err_code为0则成功
+    if res.json()["err_code"] == 0:
+        print("闯关话题状态成功")
     else:
         print(res.json())
         print("请检查t的值是否获取正确!")
@@ -53,14 +76,21 @@ if __name__ == '__main__':
 
     for i in range(cycle_count):
         print(f"...第{i + 1}次开始完成闯关...")
+        interval_time = random.randint(2, 6)
+        print(f"生成随机时间间隔，防止游戏服务器接口限流导致失败 : {interval_time} s")
         if cost_time == -1:
             cost_time = random.randint(1, 3600)
-            print(f"生成随机完成耗时:{cost_time} s")
+            print(f"生成随机闯关完成耗时: {cost_time} s")
         try:
-            finish_game(1, cost_time)
+            if sheep_type == 1:
+                finish_game_sheep(1, cost_time)
+            time.sleep(interval_time)
+            if topic_type == 1:
+                finish_game_topic(1, cost_time)
         except Exception as e:
-            print(f"游戏服务器响应超时或崩溃中未及时响应，缓缓吧，等待服务器恢复后再试！本次程序运行结束，错误日志: {e}")
+            print(f"游戏服务器响应超时或崩溃中未及时响应，缓缓吧，等待服务器恢复后再试！已经本次程序运行结束，错误日志: {e}")
             print("【羊了个羊一键闯关开始结束】")
             sys.exit(0)
         print(f"...第{i + 1}次完成闯关...")
+        time.sleep(interval_time)
     print("【羊了个羊一键闯关开始结束】")
