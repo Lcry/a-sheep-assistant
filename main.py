fc90026d6e3604f9cc473e6afea2d1f228526bcc
Lcry <i@51it.wang>
2022-09-16 22:50:26 +0800

    代码死不罢休达到目标次数，高亮提示，且行且珍惜

--- main.py
+++ main.py
@@ -29,7 +29,8 @@ request_header = {
     "User-Agent": header_user_agent,
     "t": header_t,
     "Referer": "https://servicewechat.com/wx141bfb9b73c970a9/17/page-frame.html",
-    "Accept-Encoding": "gzip,compress,br,deflate"
+    "Accept-Encoding": "gzip,compress,br,deflate",
+    "Connection": "close"
 }
 
 """
@@ -40,10 +41,12 @@ Parameters:
 
 
 def finish_game_sheep(skin, rank_time):
+    s = requests.session()
+    s.keep_alive = False
     res = requests.get(finish_sheep_api % (rank_time, skin), headers=request_header, timeout=10, verify=True)
     # err_code为0则成功
     if res.json()["err_code"] == 0:
-        print("闯关羊群状态成功")
+        print("\033[1;36m恭喜你! 本次闯关羊群状态成功\033[0m")
     else:
         print(res.json())
         print("请检查t的值是否获取正确!")
@@ -57,25 +60,29 @@ Parameters:
 
 
 def finish_game_topic(skin, rank_time):
+    s = requests.session()
+    s.keep_alive = False
     res = requests.get(finish_topic_api % (rank_time, skin), headers=request_header, timeout=10, verify=True)
     # err_code为0则成功
     if res.json()["err_code"] == 0:
-        print("闯关话题状态成功")
+        print("\033[1;36m恭喜你! 本次闯关话题状态成功\033[0m")
     else:
         print(res.json())
         print("请检查t的值是否获取正确!")
 
 
 if __name__ == '__main__':
-    print("【羊了个羊一键闯关开始启动】")
+    print("【羊了个羊一键闯关启动】")
     # 前置判断，程序员何必为难程序员呢，针对恶意刷次数对服务器造成压力的进行拦截
     if cycle_count > 10:
         print("程序员何必为难程序员,请勿恶意刷次数对服务器造成压力，请设定cycle_count的值小于10以下的值，本次程序运行结束")
         print("【羊了个羊一键闯关开始结束】")
         sys.exit(0)
 
-    for i in range(cycle_count):
-        print(f"...第{i + 1}次开始完成闯关...")
+    i = 1
+    success = 0
+    while True:
+        print(f"...第{i}次尝试完成闯关...")
         interval_time = random.randint(2, 6)
         print(f"生成随机时间间隔，防止游戏服务器接口限流导致失败 : {interval_time} s")
         if cost_time == -1:
@@ -84,13 +91,14 @@ if __name__ == '__main__':
         try:
             if sheep_type == 1:
                 finish_game_sheep(1, cost_time)
+                success += 1
             time.sleep(interval_time)
             if topic_type == 1:
                 finish_game_topic(1, cost_time)
+                success += 1
         except Exception as e:
-            print(f"游戏服务器响应超时或崩溃中未及时响应，缓缓吧，等待服务器恢复后再试！已经本次程序运行结束，错误日志: {e}")
-            print("【羊了个羊一键闯关开始结束】")
-            sys.exit(0)
-        print(f"...第{i + 1}次完成闯关...")
-        time.sleep(interval_time)
-    print("【羊了个羊一键闯关开始结束】")
+            print(f"游戏服务器响应超时或崩溃中未及时响应，缓缓吧，等待服务器恢复后再试！本次失败请忽略，错误日志: {e}")
+        if success == cycle_count:
+            print("【羊了个羊一键闯关结束】")
+        print(f"\033[4;32m已成功完成{success}次\033[0m")
+        i += 1
